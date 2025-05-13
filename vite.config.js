7cb68ba8027f59b072b1b81c39e42608754bb1da
Chris Danielson <christopher.a.danielson@protonmail.com>
2025-05-13 11:24:17 -0400

    modify 'base' property in vite.config.js and 'homepage' property in package.json

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: "/wes-personal-training/",
+  base: "/wes-personal-training",
 })
