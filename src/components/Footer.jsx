2bc8cdabb87103cc64621f45ac157088cd6d7c1b
Chris Danielson <christopher.a.danielson@protonmail.com>
2025-05-13 11:26:40 -0400

    Updates

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-200 text-center p-4">
-      <p>© {new Date().getFullYear()} MyBrand. All rights reserved.</p>
-    </footer>
-  );
-}
-
