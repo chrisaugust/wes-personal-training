281c13ed71bf63aec6e90bf8515453a2ec5e5b1d
Chris Danielson <christopher.a.danielson@protonmail.com>
2025-05-13 11:50:27 -0400

    Update 'base' property in vite.config.js; update 'homepage' property in package.json

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/wes-personal-training",
   plugins: [react(), tailwindcss(),]
 })
